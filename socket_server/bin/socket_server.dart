cdb7303654fadf81b9beaa830a811a42dfae36c3
Fernanda Dias <fernandasd26@gmail.com>
2021-07-27 16:14:51 -0300

    updated

--- socket_server/bin/socket_server.dart
+++ socket_server/bin/socket_server.dart
@@ -5,7 +5,8 @@ import 'fake.dart';
 
 void main() async {
   // bind the socket server to an address and port
-  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 4567);
+  final port = int.parse(Platform.environment['PORT'] ?? '4567');
+  final server = await ServerSocket.bind(InternetAddress.anyIPv4, port);
 
   // listen for clent connections to the server
   server.listen((client) {
