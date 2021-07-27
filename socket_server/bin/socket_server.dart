722a0f53b87d3cca59f8889f22408a59bdedaf69
Fernanda Dias <fernandasd26@gmail.com>
2021-07-27 15:21:57 -0300

    Creating socket server

---
+++ socket_server/bin/socket_server.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'fake.dart';
+
+void main() async {
+  // bind the socket server to an address and port
+  final server = await ServerSocket.bind(InternetAddress.anyIPv4, 4567);
+
+  // listen for clent connections to the server
+  server.listen((client) {
+    handleConnection(client);
+  });
+}
+
+void handleConnection(Socket client) {
+  print('Connection from'
+      ' ${client.remoteAddress.address}:${client.remotePort}');
+
+  client.listen(
+    (Uint8List data) async {
+      await Future.delayed(Duration(seconds: 1));
+      // final message = String.fromCharCodes(data);
+
+      client.write(fakeResponse);
+      // await client.close();
+    },
+
+    // handle errors
+    onError: (error) {
+      print(error);
+      client.close();
+    },
+
+    // handle the client closing the connection
+    onDone: () {
+      print('Client left');
+      client.close();
+    },
+  );
+}
